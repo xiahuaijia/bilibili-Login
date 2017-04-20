9d7696e9801a1eea8eb8481658baa6d6e0064ec9
AceSheep <1012993608@qq.com>
2017-04-20 15:03:00 +0800

    创建文件

---
+++ js/main.js
@@ -0,0 +1,26 @@
+﻿function load_publicKey(){
+$.getJSON("/bilibili_Login/ajax.php?act=getkey&ts=" + new Date().getTime(), function (rs) {
+	if (rs && rs.error) {
+		console.info("服务端出现异常，请稍后重试");
+	} else {
+		  var jscrypt = new JSEncrypt();
+		  jscrypt.setPublicKey(rs.key);
+		  Basepasswd = getQueryString("passwd");
+		  sign = checksign(getQueryString("sign"),Basepasswd);
+		  if (Basepasswd && sign) {
+		  	passwd = utf8to16(base64decode(Basepasswd));
+		  	/*console.info(passwd);*/
+		  	passwd = jscrypt.encrypt(rs.hash + passwd);
+		  	/*console.info(passwd);*/
+		  	document.write(passwd);
+		  }else{
+		  	document.write("参数错误");
+		  }
+		}
+	})
+}
+function getQueryString(name) { 
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+	var r = window.location.search.substr(1).match(reg); 
+	if (r != null) return unescape(r[2]); return null; 
+}
